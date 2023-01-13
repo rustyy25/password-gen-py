b1f3b5b2a807cc265b3a146242fe32ce95bd0e2d
John Giomilas <121767354+johngiomilas@users.noreply.github.com>
2023-01-13 15:18:29 +0200

    Add files via upload

---
+++ passgen.py
@@ -0,0 +1,19 @@
+import random
+import time
+import string
+
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+length = int(input("Provide a length: "));
+chars = letters + digits + symbols
+password = "";
+
+
+for i in range(length):
+    password = password + random.choice(chars);
+    print('Generating your password...');
+    time.sleep(1);
+
+print(f'Your new password is: {password}');
