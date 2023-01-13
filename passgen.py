17afdce4cd90e0d497e438aeb54799afe39f02a2
John Giomilas <121767354+johngiomilas@users.noreply.github.com>
2023-01-13 18:58:45 +0200

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -10,10 +10,9 @@ length = int(input("Provide a length: "));
 chars = letters + digits + symbols
 password = "";
 
-
 for i in range(length):
     password = password + random.choice(chars);
-    print('Generating your password...');
-    time.sleep(1);
-
+   
+print('Generating your password...');
+time.sleep(5);
 print(f'Your new password is: {password}');
