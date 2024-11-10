e25a8aec0c8698ad1812f033f75abc62b3f5cb4c
Rusty <121767354+rustyy25@users.noreply.github.com>
2024-11-10 15:18:04 +0200

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -7,12 +7,12 @@ symbols = string.punctuation
 
 length = int(input("Provide a length: "))
 amount = int(input("Enter the amount of password to generate: "))
-chars = letters + digits + symbols
+all = letters + digits + symbols
 
 for i in range(amount):
     pwd = ""
 
     for j in range(length):
-        pwd += random.choice(chars)
+        pwd += random.choice(all)
         
-    print(f'Your new pwd is: {pwd}')
+    print(f'Your new password is: {pwd}')
